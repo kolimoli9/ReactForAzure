65ee8183db8d3e76e900e11b1c03f2e65a180751
Yahav Sabag <yahavsabag9@gmail.com>
2022-08-18 05:27:31 +0300

    somehow the app jumped back 1 version release

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -73,7 +73,7 @@ const forgotPwd = async ()=>{
   return (
     <div className='login'>
 		{/* Start Modal */}
-		<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+		<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
